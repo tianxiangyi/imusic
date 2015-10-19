3a4a43d8fdef3be39e66613d84f5accd723df601
田向毅 <270935609@qq.com>
2015-10-19 16:09:30 +0800

    Create Resource.h

---
+++ Resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by imusic.rc
+//
+#define IDC_FILE                        3
+#define IDC_PLAY                        4
+#define ID_Exit                         6
+#define IDC_Exit                        6
+#define IDD_IMUSIC_DIALOG               102
+#define IDR_MAINFRAME                   128
+#define IDC_DISPFILE                    1000
+#define IDC_STOP                        1001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
